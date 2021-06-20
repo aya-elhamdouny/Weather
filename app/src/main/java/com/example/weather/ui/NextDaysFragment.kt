1ed5c89aa496ba69f0e1edfc3f35246b23f461b7
aya-elhamdouny <52864131+aya-elhamdouny@users.noreply.github.com>
2021-06-20 17:42:34 +0200

    -home screen -next days screen -day item -hour item -detail screen

---
+++ app/src/main/java/com/example/weather/ui/NextDaysFragment.kt
@@ -0,0 +1,13 @@
+package com.example.weather.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weather.R
+
+
+class NextDaysFragment : Fragment(R.layout.fragment_next_days) {
+
+}
